3ec92da987248a7b65bd4bcc4b4456266abc9c65
LaurenGifford <lauren.gifford.17@gmail.com>
2021-03-20 22:38:34 -0400

    auth without google

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,40 @@
 class ApplicationController < ActionController::API
     # before_action :authenticate
 
-    # def encode_token(payload)
-    #   JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
-    # end
+    def encode_token(payload)
+      JWT.encode(payload, '=$47X`:pg{_nvFU', 'HS256')
+    end
   
-    # def decode_token(token)
-    #   JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorthim: 'HS256' })[0]
-    # end
+    def decode_token(token)
+      JWT.decode(token, '=$47X`:pg{_nvFU', true, { algorthim: 'HS256' })[0]
+    end
+
+    # secret? '=$47X`:pg{_nvFU'
   
-    # def authenticate
-    #   # JWT.decode will throw an error if decoding doesn't succeed
-    #   # so we need to handle the error so our app doesn't crash
-    #   begin
-    #     # decode token using JWT library
-    #     payload = decode_token(get_auth_token)
+    def authenticate
+      # JWT.decode will throw an error if decoding doesn't succeed
+      # so we need to handle the error so our app doesn't crash
+      begin
+        # decode token using JWT library
+        payload = decode_token(get_auth_token)
   
-    #     # get the user_id from the decoded token and use it to
-    #     # set an instance variable for the current user
-    #     set_current_user!(payload["teammate_id"])
-    #   rescue
-    #     render json: { error: "Invalid Request" }, status: :unauthorized
-    #   end
-    # end
+        # get the user_id from the decoded token and use it to
+        # set an instance variable for the current user
+        set_current_user!(payload["teammate_id"])
+      rescue
+        render json: { error: "Invalid Request" }, status: :unauthorized
+      end
+    end
   
-    # private
+    private
   
-    # def get_auth_token
-    #   auth_header = request.headers['Authorization']    
-    #   auth_header.split(' ')[1] if auth_header
-    # end
+    def get_auth_token
+      auth_header = request.headers['Authorization']    
+      auth_header.split(' ')[1] if auth_header
+    end
   
-    # def set_current_user!(id)
-    #   @current_user = Teammate.find(id)
-    # end
+    def set_current_user!(id)
+      @current_user = Teammate.find(id)
+    end
 
 end
--- app/controllers/teammates_controller.rb
+++ app/controllers/teammates_controller.rb
@@ -1,5 +1,10 @@
 class TeammatesController < ApplicationController
-    # skip_before_action :authenticate, only: [:create, :login, :google_login, :show]
+    # skip_before_action :authenticate, only: [:create, :login, :google_login]
+
+    def index
+        teammates = Teammate.all
+        render json: teammates
+    end
 
     def show
         teammate = Teammate.find(params[:id])
@@ -12,54 +17,56 @@ class TeammatesController < ApplicationController
         render json: tm
     end
 
-    # def create
-    #     user = Teammate.create(
-    #       name: params[:name],
-    #       password: params[:password],
-    #     )
+    def create
+        teammate = Teammate.create(
+          name: params[:name],
+          password: params[:password],
+          points: params[:points],
+          team_id: params[:team_id]
+        )
     
-    #     if user.valid?
-    #       token = encode_token({ teammate_id: user.id })
+        if teammate.valid?
+          token = encode_token({ teammate_id: teammate.id })
     
-    #       render json: { user: TeammateSerializer.new(user), token: token }, status: :created
-    #     else
-    #       render json: { error: user.errors.full_messages }, status: :bad_request
-    #     end
-    # end
+          render json: { teammate: TeammateSerializer.new(teammate), token: token }, status: :created
+        else
+          render json: { error: teammate.errors.full_messages }, status: :bad_request
+        end
+    end
       
     
-    # def login
-    #     user = Teammate.find_by(name: params[:name])
-    #     if user && user.authenticate(params[:password])
-    #       token = encode_token({ teammate_id: user.id })
-    #       render json: { user: TeammateSerializer.new(user), token: token }
-    #     else
-    #       render json: { error: "Invalid username or password" }, status: :unauthorized
-    #     end
-    # end
+    def login
+        teammate = Teammate.find_by(name: params[:name])
+        if teammate && teammate.authenticate(params[:password])
+          token = encode_token({ teammate_id: teammate.id })
+          render json: { teammate: TeammateSerializer.new(teammate), token: token }
+        else
+          render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
     
       # before_action :authenticate
-    #   def autologin
-    #     render json: @current_user
-    #   end
+      def autologin
+        render json: @current_user
+      end
     
 
-    # def google_login
-    #     payload = get_google_token_payload
-    #     if payload
-    #       # find/create user from payload (this will be a new method in the User model)
-    #       user = Teammate.from_google_signin(payload)
-    #       if user
-    #         # save user_id in token so we can use it in future requests
-    #         token = encode_token({ teammate_id: user.id })
-    #         # send token and user in response
-    #         render json: { user: TeammateSerializer.new(user), token: token }
-    #         return
-    #       end
-    #     end
-    #         # for invalid requests, send error messages to the front end
-    #     render json: { message: "Could not log in" }, status: :unauthorized
-    # end
+    def google_login
+        payload = get_google_token_payload
+        if payload
+          # find/create user from payload (this will be a new method in the User model)
+          teammate = Teammate.from_google_signin(payload)
+          if teammate
+            # save teammate_id in token so we can use it in future requests
+            token = encode_token({ teammate_id: teammate.id })
+            # send token and teammate in response
+            render json: { teammate: TeammateSerializer.new(teammate), token: token }
+            return
+          end
+        end
+            # for invalid requests, send error messages to the front end
+        render json: { message: "Could not log in" }, status: :unauthorized
+    end
 
     private
 
@@ -67,21 +74,21 @@ class TeammatesController < ApplicationController
         params.require(:teammate).permit(:name, :points, :password, :team_id)
     end
 
-    # def get_google_token_payload
-    #     if request.headers["Authorization"]
-    #       # extract the token from the Authorization header
-    #       token_id = request.headers["Authorization"].split(" ")[1]
+    def get_google_token_payload
+        if request.headers["Authorization"]
+          # extract the token from the Authorization header
+          token_id = request.headers["Authorization"].split(" ")[1]
     
-    #       # this is the code from the Google auth gem
-    #       validator = GoogleIDToken::Validator.new
-    #       begin
+          # this is the code from the Google auth gem
+          validator = GoogleIDToken::Validator.new
+          begin
     
-    #         # check the token_id and return the payload
-    #         # make sure your .env file has a matching key
-    #         validator.check(token_id, ENV["GOOGLE_OAUTH_CLIENT_ID"])
-    #       rescue GoogleIDToken::ValidationError => e
-    #         p "Cannot validate: #{e}"
-    #       end
-    #     end
-    # end
+            # check the token_id and return the payload
+            # make sure your .env file has a matching key
+            validator.check(token_id, ENV["GOOGLE_OAUTH_CLIENT_ID"])
+          rescue GoogleIDToken::ValidationError => e
+            p "Cannot validate: #{e}"
+          end
+        end
+    end
 end
